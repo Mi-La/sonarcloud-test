38b4dd8ef41a8d86f4ef1a0b933919ba26fae9e6
Milan Kriz <milan.kriz@eccam.com>
2024-06-05 20:38:41 +0200

    Add first rule

--- main.cpp
+++ main.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
 #include <stdint.h>
 
+template <typename... T>
+void unused(T...)
+{}
+
+// Signed and unsigned types should not be mixed in expressions
+void s845()
+{
+    // official example
+    {
+        int32_t a = 10;
+        uint32_t b = a; // noncompliant
+        int32_t c = (a > 0) ? a : b; // noncompliant
+        if (a > b) // noncompliant
+        {
+            a = c;
+        }
+        unused(a, b, c);
+    }
+
+    // 16-bit
+    {
+        int16_t a = 10;
+        uint16_t b = a; // noncompliant
+        int16_t c = (a > 0) ? a : b; // noncompliant
+        if (a > b) // noncomplient
+        {
+            a = c;
+        }
+        unused(a, b, c);
+    }
+}
+
+// Results of ~ and << operations on operands of underlying types unsigned char and unsigned short should immediately be cast to the operand's underlying type
+void s853()
+{
+    // TBD
+}
+
 int main(int argc, char* argv[])
 {
-    uint16_t value = 10;
-
-    uint16_t result = value | value;
-
-    std::cout << uint32_t(result) << std::endl;
+    s845();
+    s853();
 
     return 0;
 }
