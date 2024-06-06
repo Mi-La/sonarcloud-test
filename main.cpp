820df827176e275310d555009cd88072c7a60b42
Milan Kriz <milan.kriz@eccam.com>
2024-06-06 14:30:09 +0200

    extend s845

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ void s845()
         int16_t a = 10;
         uint16_t b = a; // noncompliant
         int16_t c = (a > 0) ? a : b; // noncompliant
-        if (a > b) // non-compliant - doesn't work probably due to integral promotions?
+        if (a > b) // noncompliant - doesn't work probably due to integral promotions?
         {
             a = c;
         }
@@ -36,7 +36,7 @@ void s845()
     {
         int32_t a = 10;
         uint16_t b = 11;
-        int32_t c  = (a > b) ? a : b; // non-compliant - doesn't work probably due to integral promotions?
+        int32_t c  = (a > b) ? a : b; // noncompliant - doesn't work probably due to integral promotions?
 
         unused(a, b, c);
     }
@@ -45,6 +45,7 @@ void s845()
     {
         uint16_t a = 0x00FF;
         uint8_t b = 8;
+        // a << b will become 'int' due to integral promotions, why is it ok to assign int to uint16_t without cast?
         uint16_t c = a << b;
 
         unused(a, b, c);
@@ -55,6 +56,10 @@ void s845()
         uint16_t a = 0x0F;
         uint16_t b = 0xF0;
         uint16_t c = 0xFF00;
+        // a | b will become 'int' due to integral promotions
+        // - why is it ok to do or with c without cast
+        // a | b | c will become 'int' due to integral promotions
+        // - why is it ok to assign to int to uint16_t without cast?
         uint16_t d = a | b | c;
 
         unused(a, b, c, d);
@@ -64,7 +69,15 @@ void s845()
 // Results of ~ and << operations on operands of underlying types unsigned char and unsigned short should immediately be cast to the operand's underlying type
 void s853()
 {
-    // TBD
+    // original example
+    {
+        uint8_t port = 0x5aU;
+        uint8_t result_8;
+        uint16_t result_16;
+        uint16_t mode;
+        result_8 = (~port) >> 4; // noncompliant
+        result_16 = ((port << 4) & mode) >> 6; // noncompliant
+    }
 }
 
 int main(int argc, char* argv[])
