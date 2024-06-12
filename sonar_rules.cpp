fd3e0a9188e1211784e87d4eb3435e38af3dc351
Milan Kriz <milan.kriz@eccam.com>
2024-06-12 08:53:41 +0200

    Add consts to sonar rules

--- sonar_rules.cpp
+++ sonar_rules.cpp
@@ -7,8 +7,8 @@ static void s845()
     // official example
     {
         int32_t a = 10;
-        uint32_t b = a; // noncompliant
-        int32_t c = (a > 0) ? a : b; // noncompliant
+        const uint32_t b = a; // noncompliant
+        const int32_t c = (a > 0) ? a : b; // noncompliant
         if (a > b) // noncompliant
         {
             a = c;
@@ -18,8 +18,8 @@ static void s845()
     // 16-bit
     {
         int16_t a = 10;
-        uint16_t b = a; // noncompliant
-        int16_t c = (a > 0) ? a : b; // noncompliant
+        const uint16_t b = a; // noncompliant
+        const int16_t c = (a > 0) ? a : b; // noncompliant
         if (a > b) // noncompliant - doesn't work probably due to integral promotions?
         {
             a = c;
@@ -28,35 +28,36 @@ static void s845()
 
     // mix signed 32 and unsigned 16
     {
-        int32_t a = 10;
-        uint16_t b = 11;
-        int32_t c  = (a > b) ? a : b; // noncompliant - doesn't work probably due to integral promotions?
+        const int32_t a = 10;
+        const uint16_t b = 11;
+        const int32_t c  = (a > b) ? a : b; // noncompliant - doesn't work probably due to integral promotions?
 
         (void)c;
     }
 
     // shift unsigned 16 using unsigned 8
     {
-        uint16_t a = 0x00FF;
-        uint8_t b = 8;
+        const uint16_t a = 0x00FF;
+        const uint8_t b = 8;
         // a << b will become 'int' due to integral promotions, why is it ok to assign int to uint16_t without cast?
-        uint16_t c = a << b;
+        const uint16_t c = a << b;
 
         (void)c;
     }
 
     // multiple binary or-s
     {
-        uint16_t a = 0x0F;
-        uint16_t b = 0xF0;
-        uint16_t c = 0xFF00;
+        const uint16_t a = 0x0F;
+        const uint16_t b = 0xF0;
+        const uint16_t c = 0xFF00;
         // a | b will become 'int' due to integral promotions
         // - why is it ok to do or with c without cast
         // a | b | c will become 'int' due to integral promotions
         // - why is it ok to assign to int to uint16_t without cast?
-        uint16_t d = a | b | c;
-        uint16_t e = static_cast<uint16_t>(a | b) | c;
+        const uint16_t d = a | b | c;
+        const uint16_t e = static_cast<uint16_t>(a | b) | c;
 
+        (void)d;
         (void)e;
     }
 }
@@ -66,12 +67,15 @@ static void s853()
 {
     // original example
     {
-        uint8_t port = 0x5aU;
+        const uint8_t port = 0x5aU;
         uint8_t result_8;
         uint16_t result_16;
-        uint16_t mode;
+        const uint16_t mode = 0x0f;
         result_8 = (~port) >> 4; // noncompliant
         result_16 = ((port << 4) & mode) >> 6; // noncompliant
+
+        (void)result_8;
+        (void)result_16;
     }
 }
 
@@ -85,14 +89,14 @@ static void s874()
 {
     // original example
     {
-        uint16_t a = 0xff;
-        int16_t b = 0x0f;
+        const uint16_t a = 0xff;
+        const int16_t b = 0x0f;
         if (( a & b ) == 0x1234U) // noncompliant until C++20
         {}
 
         if (~b == 0x1234U) // noncompliant until C++20
         {
-            s874_f(b);
+            (void)s874_f(b);
         }
     }
 }
